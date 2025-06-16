4fea797465d797e5aa373a3acb4ec5379e9eab5d
Kwiatek Szymon <ken1998@wp.pl>
2025-06-16 15:19:49 +0200

    chore: update Flutter package reference in Nix configuration

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-25.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.flutter327
+    pkgs.flutter
     pkgs.jdk17
     pkgs.unzip
   ];

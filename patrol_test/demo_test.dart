f66e05dec15b84fc07288370a21b2269db301506
Kwiatek Szymon <ken1998@wp.pl>
2026-02-13 16:53:08 +0100

    Add try-catch block for location permission tap in demo_test.dart

--- patrol_test/demo_test.dart
+++ patrol_test/demo_test.dart
@@ -24,6 +24,12 @@ void main() {
 
       await $.platform.mobile
           .tapOnNotificationBySelector(Selector(textContains: 'Tap'));
+      try {
+        await $.platform.android
+            .tap(AndroidSelector(textContains: 'Turn on location'));
+      } catch (e) {
+        // ignore exception
+      }
 
       await $(RegExp('Your location')).waitUntilVisible(
         timeout: const Duration(seconds: 30),

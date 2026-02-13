667b48f05610b732ac899313dda6e1ea55627ed6
Kwiatek Szymon <ken1998@wp.pl>
2026-02-13 15:59:38 +0100

    Revert "change channel for newer"
    This reverts commit 9a35d7aa0ea31ea8223283154d91f9e80a832b6d.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-25.11"; # or "unstable"
+  channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.jdk17
@@ -20,7 +20,14 @@
     ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {};
+      onCreate = {
+        # Upgrade Flutter to version 3.38.4 to match pubspec.yaml
+        upgrade-flutter = ''
+          flutter channel stable
+          flutter upgrade --force
+          flutter --version
+        '';
+      };
 
       onStart = {
         default.openFiles = [ "patrol_test/demo_test.dart" ];
